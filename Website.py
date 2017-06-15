62e0f98f2c65677104eb51562365d90842f4d78c
kedepo <depotterkenneth@outlook.com>
2017-06-15 14:52:01 +0200

    fixed css

--- Website.py
+++ Website.py
@@ -1,16 +1,33 @@
 from flask import Flask
+from flask import render_template
 import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def live():
+    return render_template('index.html')
 
+@app.route('/history')
+def history():
+    return render_template('history.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+@app.errorhandler(401)
+def throw_401(error):
+    return render_template('error_pages/401.html', error=error)
+
+
+@app.errorhandler(404)
+def throw_404(error):
+    return render_template('error_pages/404.html', error=error)
 
 if __name__ == '__main__':
     # check omgevingsvariabele voor poort of neem 8080 als standaarc
     port = int(os.environ.get("PORT", 8080))
-    host = "0.0.0.0" #luistert naar alle IP's i.p.v. enkel 127.0.0.1
+    host = "169.254.10.111" #luistert naar alle IP's i.p.v. enkel 127.0.0.1
     app.run(host=host, port=port, debug=True)
